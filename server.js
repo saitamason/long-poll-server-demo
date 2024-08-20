91d2f1f37764b469050a55ad2f7ebdad59972a2b
saitamason <92246829+saitamason@users.noreply.github.com>
2024-08-20 17:00:47 +0200

    feat: add SSE example

--- server.js
+++ server.js
@@ -10,9 +10,23 @@ app.use(bodyParser.json());
 
 app.get("/messages", (req, res) => {
   console.log("Waiting for new messages...");
-  messageEventEmitter.once("newMessage", (message) => {
+
+  // Set headers for server-sent events
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+  // Remove above for long polling only
+
+  // For long polling only
+  // messageEventEmitter.once("newMessage", (message) => {
+  //   console.log("Message received:", message);
+  //   res.json({ message });
+  // });
+
+  // For server-sent events
+  messageEventEmitter.on("newMessage", (message) => {
     console.log("Message received:", message);
-    res.json({ message });
+    res.write(`data: ${JSON.stringify({ message })}\n\n`);
   });
 });
 
